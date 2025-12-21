b9e9192b7ff4ffe58df87cc76a0a46f07a07b634
OneDuckyBoy <stilianmatev@gmail.com>
2025-12-21 23:54:03 +0200

    started working on user interface and finished the logic of drawing cards and first wild card scenario

--- uno_project/uno_project.cpp
+++ uno_project/uno_project.cpp
@@ -8,30 +8,39 @@
 
 const char FILENAME[] = "UnoSaveState.txt";
 
-const int RED_COLOR_CODE = 91;
-const int GREEN_COLOR_CODE = 92;
-const int YELLOW_COLOR_CODE = 93;
-const int BLUE_COLOR_CODE = 94;
-const int MAGENTA_COLOR_CODE = 95;
-int currentDrawDeckId = 0;
-const int MAX_DECK_SIZE = 108;
-
 struct card
 {
     char color;
     char value[8];
     char text[16];
 };
+const int RED_COLOR_CODE = 91;
+const int GREEN_COLOR_CODE = 92;
+const int YELLOW_COLOR_CODE = 93;
+const int BLUE_COLOR_CODE = 94;
+const int MAGENTA_COLOR_CODE = 95;
+const int MAX_DECK_SIZE = 108;
+
+struct card drawDeck[MAX_DECK_SIZE];
+int currentDrawDeckId = 0;
+
+
+struct card discardDeck[MAX_DECK_SIZE];
+int topDiscardDeckId = 0;
+
 struct player
 {
     struct card hand[MAX_DECK_SIZE];
+    int handSize = 7;
 };
 const int MAX_PLAYERS = 4;
 struct player players[MAX_PLAYERS];
+int currentPlayerId = 0;
 
-struct card drawDeck[MAX_DECK_SIZE];
-struct card discardDeck[MAX_DECK_SIZE];
 void fillUnoDeck(struct card drawDeck[MAX_DECK_SIZE]);
+int main();
+void displayCurrentPlayerHand(player& currentPlayer);
+void FillPlayersHands(int numberOfPlayers);
 void shuffleDeck(struct card drawDeck[MAX_DECK_SIZE], int arrSize, std::mt19937& gen);
 
 void ClearConsole();
@@ -46,14 +55,22 @@ const int COLORS_SIZE = 4;
 const int colorCodes[] = { RED_COLOR_CODE, GREEN_COLOR_CODE, YELLOW_COLOR_CODE, BLUE_COLOR_CODE };
 const char wildColor = 'W';
 const int wildColorCode = MAGENTA_COLOR_CODE;
-const int startingNumberOfCards = 7;
+const int STARTING_NUMBER_OF_CARDS = 7;
+          
 
+void colorInCard(struct card currentCard);
 
-
+bool checkIfPlayerCanPlayCard(struct card currentCard, struct card playerCard);
+bool charEquals(const char a[], const char b[], int size);
 int main()
 {
     std::random_device rd;
-    std::mt19937 gen(rd());
+    unsigned int seed = rd();
+    
+    std::mt19937 gen(seed);
+    //       597823264      //wild card is top discard card when 2 players (reshuffles deck and chooses new card)
+    //gen.seed(8);
+    //std::cout << "seed: "<<seed << std::endl;
     //std::cout << "Hello World!\n";
 
     std::cout << "Welcome to Uno tm console edition :D" << std::endl
@@ -89,10 +106,50 @@ int main()
 
         fillUnoDeck(drawDeck);
         shuffleDeck(drawDeck, MAX_DECK_SIZE, gen);
-        for (int i = 0; i < 10; i++)
+
+        FillPlayersHands(numberOfPlayers);
+
+        std::cout << "Current player: " << currentPlayerId << std::endl;
+        std::cout << "Current card: ";
+        
+        if (drawDeck[currentDrawDeckId].color==wildColor)
         {
-            std::cout << drawDeck[i].text <<std::endl;
+            //std::cout << "WIIIILD!!!"
+            do
+            shuffleDeck(drawDeck + currentDrawDeckId, MAX_DECK_SIZE - currentDrawDeckId, gen);
+            while (drawDeck[currentDrawDeckId].color == wildColor);
+
         }
+        discardDeck[topDiscardDeckId] = drawDeck[currentDrawDeckId++];
+
+        struct card currentCard = discardDeck[topDiscardDeckId];
+        colorInCard(currentCard);
+        std::cout << std::endl << "Your hand: " << std::endl;
+        struct player currentPlayer = players[currentPlayerId];
+        bool isValidOption = false;
+        do{
+        do
+        {
+            displayCurrentPlayerHand(currentPlayer);
+            std::cout << "[" << currentPlayer.handSize << "] Draw card" << std::endl;
+            std::cout << "[" << currentPlayer.handSize+1 << "] Save game and exit" << std::endl;
+            std::cout << "What do you want to do?" << std::endl;
+            std::cin >> userAnswer;
+        } while (!(userAnswer>=0&&userAnswer<=currentPlayer.handSize+1));
+        if (userAnswer == currentPlayer.handSize)
+        {
+            isValidOption = true;
+        }
+        else if (userAnswer == currentPlayer.handSize+1)
+        {
+            isValidOption = true;
+        }
+        else {
+            struct card playerCard = currentPlayer.hand[userAnswer];
+            isValidOption = checkIfPlayerCanPlayCard(currentCard, playerCard);
+        }
+        } while (!isValidOption);
+
 
     }
     else if (userAnswer == 1)
@@ -128,6 +185,73 @@ int main()
 
 
 
+}
+bool charEquals(const char a[], const char b[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (a[i] != b[i]) {
+            return false; // веднага връщаме false, ако различни
+        }
+        if (a[i] == '\0' && b[i] == '\0') {
+            return true; // края на двата стринга е достигнат
+        }
+    }
+    // ако стигнем до края на масива без различия
+    return true;
+}
+bool checkIfPlayerCanPlayCard(struct card currentCard, struct card playerCard) {
+    if (playerCard.color==wildColor)
+    {
+        return true;
+    }
+    if (currentCard.color == playerCard.color)
+    {
+        return true;
+    }
+    if (charEquals(currentCard.value, playerCard.value,8))
+    {
+        return true;
+    }return false;
+}
+
+void displayCurrentPlayerHand(player& currentPlayer)
+{
+    for (int i = 0; i < currentPlayer.handSize; i++)
+    {
+        std::cout << "[" << i << "] ";
+        colorInCard(currentPlayer.hand[i]);
+        std::cout << " ";
+    }
+}
+void colorInCard(struct card currentCard) {
+
+    char color = currentCard.color;
+    
+    for (int i = 0; i < COLORS_SIZE; i++)
+    {
+        if (color == colors[i])
+        {
+            PrintTextInColor(currentCard.text, colorCodes[i]);
+            return;
+        }
+    }
+    if (color == wildColor)
+    {
+        PrintTextInColor(currentCard.text, MAGENTA_COLOR_CODE);
+    }
+}
+
+void FillPlayersHands(int numberOfPlayers)
+{
+    for (int i = 0; i < numberOfPlayers; i++)
+    {
+        struct player currentPlayer;
+        for (int i = 0; i < STARTING_NUMBER_OF_CARDS; i++)
+        {
+            currentPlayer.hand[i] = drawDeck[currentDrawDeckId++];
+        }
+        players[i] = currentPlayer;
+
+    }
 }
 
 
